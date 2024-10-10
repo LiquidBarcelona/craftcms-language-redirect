c259af19c6ff699be48eb05998ad9a5f8061a5ee
Marc Bernet <mbernet@gmail.com>
2024-10-10 17:36:39 +0200

    update craft 5

--- src/LanguageRedirector.php
+++ src/LanguageRedirector.php
@@ -1,42 +1,52 @@
 <?php
 namespace liquidbcn\languageredirect;
 
+use Craft;
 use craft\base\Model;
+use craft\base\Plugin;
+use craft\events\RegisterUrlRulesEvent;
+use craft\web\UrlManager;
 use liquidbcn\languageredirect\models\Settings;
-use yii\web\HttpException;
+use yii\base\Event;
 
-class LanguageRedirector extends \craft\base\Plugin
+class LanguageRedirector extends Plugin
 {
     public bool $hasCpSettings = true;
 
     public function init()
     {
         parent::init();
-        $this->redirect();
+
+        // Register an event handler for URL management
+        Event::on(
+            UrlManager::class,
+            UrlManager::EVENT_REGISTER_SITE_URL_RULES,
+            function (RegisterUrlRulesEvent $event) {
+                $this->redirect();
+            }
+        );
     }
 
-    protected function redirect() {
-
-        $browser = $_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? null;
-
+    protected function redirect()
+    {
+        $browser = Craft::$app->getRequest()->getHeaders()->get('accept-language');
         $default = $this->getSettings()->defaultLanguage;
         $urls = $this->getSettings()->urls;
 
-        if(!empty($urls)) {
+        if (!empty($urls)) {
             $browserLoc = new \koenster\PHPLanguageDetection\BrowserLocalization();
             $browserLoc->setAvailable(array_keys($urls))
                 ->setDefault($default)
                 ->setPreferences($browser);
-            $url = parse_url($_SERVER['REQUEST_URI']);
 
-            if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+            $url = parse_url(Craft::$app->getRequest()->getUrl());
 
-                if(empty($url['path']) || $url['path'] === '/') {
+            if (Craft::$app->getRequest()->getIsGet()) {
+                if (empty($url['path']) || $url['path'] === '/') {
                     $language = $browserLoc->detect();
-                    if(isset($urls[$language])) {
-                        header('HTTP/1.1 301 Moved Permanently');
-                        header('Location: ' . $urls[$language]);
-                        exit();
+                    if (isset($urls[$language])) {
+                        Craft::$app->getResponse()->redirect($urls[$language], 301)->send();
+                        Craft::$app->end();
                     }
                 }
             }
